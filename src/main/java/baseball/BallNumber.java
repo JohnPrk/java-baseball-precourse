27eb89726b9d3381cfcaeb1eceed3a6209633611
JohnPrk <uo3641493@gmail.com>
2022-10-19 15:42:12 +0900

    refactor : BallNumber 책임 및 상태, 행위 재정의(index 상태 생성, 생성자 생성시 사용자 입력값에 대한 유효성 검사 생성, 비교값 체크 생성, 생성자 오버로드 추가)

--- src/main/java/baseball/BallNumber.java
+++ src/main/java/baseball/BallNumber.java
@@ -1,24 +1,72 @@
 package baseball;
 
 public class BallNumber {
-    private String number;
 
-    public BallNumber(String number) {
-        if (!number.chars().allMatch(Character::isDigit)) {
-            throw new IllegalArgumentException();
-        }
-        int num = Integer.parseInt(number);
-        if (num < 1 || num > 9) {
-            throw new IllegalArgumentException();
-        }
-        this.number = number;
-    }
+  private final int index;
+  private final String number;
 
-    public BallNumber(int number) {
-        this(String.valueOf(number));
-    }
+  public BallNumber(int index, String number) {
 
-    public String getNumber() {
-        return number;
+    // 사용자 입력 값으로 문자가 아닌 숫자 여부 체크, 숫자 범위 및 인덱스 범위 체크
+    stringValidationCheck(number);
+    numberScopeCheck(number);
+    indexScopeCheck(index);
+
+    this.index = index;
+    this.number = number;
+  }
+
+  private void indexScopeCheck(int index) {
+    if (index < 0 || index > 2) {
+      throw new IllegalArgumentException("3개의 숫자만 입력해주세요.");
     }
+  }
+
+  private void numberScopeCheck(String number) {
+    int num = Integer.parseInt(number);
+
+    if (num < 1 || num > 9) {
+      throw new IllegalArgumentException("입력값으로 1-9까지의 숫자를 입력해주세요.");
+    }
+  }
+
+  private void stringValidationCheck(String number) {
+    if (!number.chars().allMatch(Character::isDigit)) {
+      throw new IllegalArgumentException("입력값에 문자가 아닌 숫자를 입력해주세요.");
+    }
+  }
+
+  public BallNumber(int index, int number) {
+    this(index, String.valueOf(number));
+  }
+
+  public int getIndex() {
+    return index;
+  }
+
+  public String getNumber() {
+    return number;
+  }
+
+  public BallStatus check(BallNumber ballNumber) {
+
+    if (ballNumber.equals(this)) {
+      return BallStatus.STRIKE;
+    }
+    if (ballNumber.ballCheck(number)) {
+      return BallStatus.BALL;
+    }
+    return BallStatus.NOTHING;
+  }
+
+  private boolean ballCheck(String number) {
+    return this.number.equals(number);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    BallNumber ballNumber = (BallNumber) obj;
+
+    return this.number.equals(ballNumber.number) && this.index == ballNumber.index;
+  }
 }
