fdd24c54647659e6a074a485c6457afc10617bae
JohnPrk <uo3641493@gmail.com>
2022-10-19 15:55:42 +0900

    refactor : 입력값에 대한 유효성 검사 추가

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -1,21 +1,31 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Console;
-
 import java.util.ArrayList;
 import java.util.List;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class User {
 
-    public String inputRestartOrExit() {
-        return Console.readLine();
+  public String inputRestartOrExit() {
+    String input = readLine();
+    if (!(input.equals("1") || input.equals("2"))) {
+      throw new IllegalArgumentException("게임을 다시 시작하려면 1의 값을, 게임을 완전히 종료하려면 2를 입력해주세요");
+    }
+    return input;
+
+  }
+
+  public BallNumbers createNumbers() {
+    List<BallNumber> number = new ArrayList<>();
+    String[] split = readLine().split("");
+    if (split.length != 3) {
+      throw new IllegalArgumentException("3자리의 숫자를 넣어주세요.");
+    }
+    for (int i = 0; i < 3; i++) {
+      number.add(new BallNumber(i, split[i]));
     }
 
-    public BallNumbers createNumbers() {
-        List<BallNumber> number = new ArrayList<>();
-        for (String s : Console.readLine().split("")) {
-            number.add(new BallNumber(s));
-        }
-        return new BallNumbers(number);
-    }
+    return new BallNumbers(number);
+  }
 }
