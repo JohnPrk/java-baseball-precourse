7f75cc27e393a1d54041b67f07098331b6002015
JohnPrk <uo3641493@gmail.com>
2022-10-19 15:52:40 +0900

    refactor : BallNumbers 책임 및 상태, 행위 재정의(생성자 생성시 사용자 입력값에 대한 유효성 검사 생성, 비교값 체크 생성)

--- src/main/java/baseball/BallNumbers.java
+++ src/main/java/baseball/BallNumbers.java
@@ -2,29 +2,72 @@ package baseball;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class BallNumbers {
-    private List<BallNumber> numbers;
-    private static final int NUMBER_SIZE = 3;
 
-    public BallNumbers(List<BallNumber> numbers) {
-        HashSet<String> duplicationCheck = new HashSet<>();
+  private static final int NUMBER_SIZE = 3;
+  private final List<BallNumber> numbers;
 
-        if (numbers.size() != NUMBER_SIZE) {
-            throw new IllegalArgumentException();
-        }
-        for (BallNumber n : numbers) {
-            duplicationCheck.add(n.getNumber());
-        }
-        if (duplicationCheck.size() != numbers.size()) {
-            throw new IllegalArgumentException();
-        }
-        this.numbers = numbers;
+  public BallNumbers(List<BallNumber> numbers) {
+
+    // 사이즈 체크 및 인덱스, 값 중복 체크
+    sizeCheck(numbers);
+    numberDuplicationCheck(numbers);
+    indexDuplicationCheck(numbers);
+
+    this.numbers = numbers;
+  }
+
+  private void sizeCheck(List<BallNumber> numbers) {
+    if (numbers.size() != NUMBER_SIZE) {
+      throw new IllegalArgumentException();
+    }
+  }
+
+  private void indexDuplicationCheck(List<BallNumber> numbers) {
+    Set<Integer> duplicationIndexCheckHash = new HashSet<>();
+    for (BallNumber number : numbers) {
+      duplicationIndexCheckHash.add(number.getIndex());
+    }
+    if (duplicationIndexCheckHash.size() != NUMBER_SIZE) {
+      throw new IllegalArgumentException();
+    }
+  }
+
+  private void numberDuplicationCheck(List<BallNumber> numbers) {
+    Set<String> duplicationNumberCheckHash = new HashSet<>();
+
+    for (BallNumber number : numbers) {
+      duplicationNumberCheckHash.add(number.getNumber());
+    }
+    if (duplicationNumberCheckHash.size() != NUMBER_SIZE) {
+      throw new IllegalArgumentException("중복되지 않는 값 3개를 입력해주세요.");
+    }
+  }
+
+  public List<BallNumber> getNumbers() {
+    return numbers;
+  }
+
+  public MatchResult count(BallNumbers ballNumbers) {
+
+    MatchResult matchResult = new MatchResult();
+    for (BallNumber ballNumber : numbers) {
+      BallStatus ballStatus = ballNumbers.count(ballNumber);
+
+      matchResult.report(ballStatus);
     }
 
-    public List<BallNumber> getNumbers() {
-        return numbers;
-    }
+    return matchResult;
+  }
 
+  public BallStatus count(BallNumber ballNumber) {
 
+    return numbers.stream()
+        .map(randomBall -> randomBall.check(ballNumber))
+        .filter(BallStatus::isNotNothing)
+        .findFirst()
+        .orElse(BallStatus.NOTHING);
+  }
 }
