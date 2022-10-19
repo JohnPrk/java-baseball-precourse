ed62d28b71d594846bbaf769f0538533a61c06fc
JohnPrk <uo3641493@gmail.com>
2022-10-19 15:59:57 +0900

    refactor : MatchResult 객체의 책임 및 상태, 행위 재정의(BallNumbers들을 비교하여 나온 결과값에 대한 책임으로 한정)

--- src/main/java/baseball/MatchResult.java
+++ src/main/java/baseball/MatchResult.java
@@ -1,46 +1,52 @@
 package baseball;
 
 public class MatchResult {
-    private static final int NUMBER_SIZE = 3;
-    private int strike;
-    private int ball;
 
-    public MatchResult compareNumbers(BallNumbers user_numbers, BallNumbers random_numbers) {
-        strike = 0;
-        ball = 0;
+  private static final int NUMBER_SIZE = 3;
+  private int strike;
+  private int ball;
 
-        for (int i = 0; i < NUMBER_SIZE; i++) {
-            for (int j = 0; j < NUMBER_SIZE; j++) {
-                if (user_numbers.getNumbers().get(i).getNumber().equals(random_numbers.getNumbers().get(j).getNumber())) {
-                    if (i == j) strike++;
-                    else ball++;
-                }
-            }
-        }
+  public MatchResult() {
+    this.strike = 0;
+    this.ball = 0;
+  }
 
-        return this;
+  public int getStrike() {
+    return strike;
+  }
+
+  public int getBall() {
+    return ball;
+  }
+
+  public boolean endGame() {
+    return this.strike == NUMBER_SIZE;
+  }
+
+  public void report(BallStatus ballStatus) {
+    if (ballStatus.isStrike()) {
+      this.strike += 1;
+    }
+    if (ballStatus.isBall()) {
+      this.ball += 1;
+    }
+  }
+
+  public String printResult() {
+    StringBuilder sb = new StringBuilder();
+    if (this.strike == 3) {
+      return "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    }
+    if (this.strike == 0 && this.ball == 0) {
+      return "낫띵";
+    }
+    if (this.strike > 0) {
+      sb.append(strike).append("스트라이크 ");
+    }
+    if (this.ball > 0) {
+      sb.append(ball).append("볼");
     }
 
-    public String printResult() {
-        StringBuilder sb = new StringBuilder();
-        if (this.strike == 3) {
-            sb.append("3스트라이크\n");
-            sb.append("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-        } else if (this.strike == 0 && this.ball == 0) {
-            sb.append("낫띵");
-        } else {
-            if (this.strike > 0) {
-                sb.append(strike + "스트라이크 ");
-            }
-            if (this.ball > 0) {
-                sb.append(ball + "볼");
-            }
-        }
-        return String.valueOf(sb);
-    }
-
-    public boolean regameOrStop() {
-        return this.strike == 3;
-    }
+    return String.valueOf(sb);
+  }
 }
-
