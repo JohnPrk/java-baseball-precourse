563906c99740d225e0c4a527c5c6c13a45e4e997
JohnPrk <uo3641493@gmail.com>
2022-10-19 16:02:58 +0900

    style : 코드 포맷팅, 리팩토링

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,27 +1,27 @@
 package baseball;
 
 public class Game {
-    static final String RESTART = "1";
 
-    public void play(BallNumbers random_numbers) {
-        User user = new User();
-        MatchResult matchResult = new MatchResult();
-        BallNumbers user_numbers;
-        boolean keepGoing = false;
+  static final String RESTART = "1";
 
-        while (!keepGoing) {
-            System.out.print("서로 다른 3자리 숫자를 입력해주세요. ");
-            user_numbers = user.createNumbers();
-            matchResult.compareNumbers(user_numbers, random_numbers);
-            System.out.println(matchResult.printResult());
-            keepGoing = matchResult.regameOrStop();
-        }
+  public void play(BallNumbers random_numbers) {
+    User user = new User();
+    BallNumbers userNumbers;
+    boolean keepGoing = false;
+
+    while (!keepGoing) {
+      System.out.print("서로 다른 3자리 숫자를 입력해주세요. ");
+      userNumbers = user.createNumbers();
+      MatchResult result = random_numbers.count(userNumbers);
+      System.out.println(result.printResult());
+      keepGoing = result.endGame();
     }
+  }
 
-    public boolean restartOrExit() {
-        User user = new User();
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+  public boolean restartOrExit() {
+    User user = new User();
+    System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 
-        return user.inputRestartOrExit().equals(RESTART);
-    }
+    return user.inputRestartOrExit().equals(RESTART);
+  }
 }
--- src/main/java/baseball/RandomBallNumberGenerator.java
+++ src/main/java/baseball/RandomBallNumberGenerator.java
@@ -1,28 +1,32 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
-
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class RandomBallNumberGenerator {
 
-    private static final int MAX_NUMBER = 9;
-    private static final int MIN_NUMBER = 1;
-    private static final int NUMBER_SIZE = 3;
+  private static final int MAX_NUMBER = 9;
+  private static final int MIN_NUMBER = 1;
+  private static final int NUMBER_SIZE = 3;
 
-    public BallNumbers createRandomNumbers() {
-        List<Integer> number_list = new ArrayList<>();
-        List<BallNumber> number_object = new ArrayList<>();
-        while (number_list.size() < NUMBER_SIZE) {
-            int num = Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
-            if (!number_list.contains(num)) {
-                number_list.add(num);
-                number_object.add(new BallNumber(num));
-            }
-        }
+  public BallNumbers createRandomNumbers() {
+    List<Integer> number_list = new ArrayList<>();
+    List<BallNumber> number_object = new ArrayList<>();
+    int index = 0;
 
-        return new BallNumbers(number_object);
+    createNumber(number_list, number_object, index);
+
+    return new BallNumbers(number_object);
+  }
+
+  private void createNumber(List<Integer> number_list, List<BallNumber> number_object, int index) {
+    while (number_list.size() < NUMBER_SIZE) {
+      int num = Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
+      if (!number_list.contains(num)) {
+        number_list.add(num);
+        number_object.add(new BallNumber(index++, num));
+      }
     }
+  }
 }
